9d98421515a0ae20bdeaa9b72d039f0ba06ed4d5
TugcenurGunduz <tnurgunduz@hotmail.com>
2021-01-26 19:19:57 +0300

    Metot Kavramı

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //naming convention
+        //syntax
+        public void Ekle(Urun urun) // Metot parametre alıyor. Urun->tipi, urun->aşağıda kullanacağım isimlendirme
+        {
+            Console.WriteLine("İşlem gerçekleşti, sepete eklendi : " + urun.Adi);
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("İşlem gerçekleşti, sepete eklendi : " + urunAdi);
+        }
+    }
+}
---
+++ Metotlar/Urun.cs
@@ -0,0 +1,13 @@
+﻿namespace Metotlar
+{
+    class Urun
+    {
+        //property - özellik
+        public int Id { get; set; } //bir datayı diğerlerinden ayırt etmek için kullanılan eşsiz bir değer -Id-.
+        public string Adi { get; set; }
+        public double Fiyati { get; set; }
+        public string Aciklama { get; set; }
+        public int StokAdedi { get; set; }
+
+    }
+}
