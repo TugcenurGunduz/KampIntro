80635ea5c402b3362bd8e9b340cb1d89b9f2fb12
TugcenurGunduz <tnurgunduz@hotmail.com>
2021-01-26 19:16:08 +0300

    Matematiksel işlem

---
+++ Matematik/DortIslem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matematik
+{
+    class DortIslem
+    {
+        //Topla(2,3)
+        public void Topla(int sayi1, int sayi2)
+        {
+            int toplam = sayi1 + sayi2;
+            Console.WriteLine("Sonuç : " + toplam);
+        }
+        public void Cikar(int sayi1, int sayi2)
+        {
+            int cikar = sayi1 - sayi2;
+            Console.WriteLine("Sonuc : " + cikar);
+        }
+
+    }
+}
