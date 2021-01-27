8224a4a474d329534b75782d029a68c73b0c3088
TugcenurGunduz <tnurgunduz@hotmail.com>
2021-01-27 14:14:07 +0300

    Dictionary sınıfı kullanılarak Add metodu örneklendirilmiştir.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    class MyDictionary<TKey, TValue>
+    {
+        public TKey[] tkeys;
+        public TValue[] tvalues;
+
+
+        public MyDictionary() //constructor
+        {
+            tkeys = new TKey[0];
+            tvalues = new TValue[0];
+
+        }
+        public void Add(TKey key, TValue value) //Eleman ekleme
+        {
+            TKey[] TempKeys = tkeys;
+            TValue[] TempValues = tvalues;
+
+            tkeys = new TKey[tkeys.Length + 1];
+            for (int i = 0; i < TempKeys.Length; i++)
+            {
+                tkeys[i] = TempKeys[i];
+            }
+            tkeys[tkeys.Length - 1] = key;
+
+            tvalues = new TValue[tvalues.Length + 1];
+            for (int i = 0; i < TempValues.Length; i++)
+            {
+                tvalues[i] = TempValues[i];
+            }
+            tvalues[tvalues.Length - 1] = value;
+        }
+
+        public TKey[] Tkeys
+        {
+            get { return tkeys; }
+        }
+
+        public TValue[] Tvalues
+        {
+            get { return tvalues; }
+        }
+
+    }
+
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> citys = new MyDictionary<int, string>();
+            citys.Add(34, "İstanbul");
+            citys.Add(06, "Ankara");
+            citys.Add(78, "Karabük");
+
+            foreach (var value in citys.tvalues)
+            {
+                Console.WriteLine(value);
+            }
+            foreach (var key in citys.tkeys)
+            {
+                Console.WriteLine(key);
+            }
+
+        }
+    }
+}
